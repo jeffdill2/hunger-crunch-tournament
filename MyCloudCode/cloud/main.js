a5b0d7f4ec549cf1a23a6b42050243484649d950
Joshua Smith <joshuawordsmith@gmail.com>
2014-06-25 19:54:17 -0400

    changed hello function output

--- MyCloudCode/cloud/main.js
+++ MyCloudCode/cloud/main.js
@@ -2,5 +2,5 @@
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
 Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+  response.success("hey hey hey");
 });
