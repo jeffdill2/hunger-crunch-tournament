6243efbc39bbddfd2edd3128a9f8c4ad13748e82
Joshua Smith <joshuawordsmith@gmail.com>
2014-06-25 21:52:55 -0400

    added test objects to server, and make a cloud function to add and average scores

--- MyCloudCode/cloud/main.js
+++ MyCloudCode/cloud/main.js
@@ -1,6 +1,52 @@
+var Test = Parse.Object.extend({
 
+	className: 'test',
+
+});
+
+function addPlayer (pl, pt) {
+
+	var test = new Parse.Object('test');
+			test.set('player', pl );
+			test.set('score', pt);
+			test.save();
+ }
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("hey hey hey");
+Parse.Cloud.define("addTestPlayer", function(request, response) {
+  response.success(addPlayer('player3', 45));
+});
+
+Parse.Cloud.define("averageScore", function(request, response) {
+  var query = new Parse.Query("test");
+  // query.equalTo("player", request.params.movie);
+  query.find({
+    success: function(results) {
+      var sum = 0;
+      for (var i = 0; i < results.length; i += 1) {
+        sum += results[i].get("score");
+      }
+      response.success(sum / results.length);
+    },
+    error: function() {
+      response.error("score lookup failed");
+    }
+  });
+});
+
+Parse.Cloud.define("totalScore", function(request, response) {
+  var query = new Parse.Query("test");
+  // query.equalTo("player", request.params.movie);
+  query.find({
+    success: function(results) {
+      var sum = 0;
+      for (var i = 0; i < results.length; i += 1) {
+        sum += results[i].get("score");
+      }
+      response.success(sum);
+    },
+    error: function() {
+      response.error("score lookup failed");
+    }
+  });
 });
