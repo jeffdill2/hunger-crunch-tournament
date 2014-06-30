47d30037b4d07a6a97f9b49e6b904541817ac19f
Joshua Smith <joshuawordsmith@gmail.com>
2014-06-30 13:40:37 -0400

    totals scores and updates server

--- MyCloudCode/cloud/main.js
+++ MyCloudCode/cloud/main.js
@@ -1,52 +1,100 @@
-var Test = Parse.Object.extend({
+var _ = require('underscore');
 
-	className: 'test',
+var TotalStats = Parse.Object.extend({
+
+	className: 'TotalStats',
 
 });
 
-function addPlayer (pl, pt) {
+// function addPlayer (pl, pt) {
 
-	var test = new Parse.Object('test');
-			test.set('player', pl );
-			test.set('score', pt);
-			test.save();
- }
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define("addTestPlayer", function(request, response) {
-  response.success(addPlayer('player3', 45));
-});
+// 	var test = new Parse.Object('test');
+// 			test.set('player', pl );
+// 			test.set('score', pt);
+// 			test.save();
+//  }
+// // Use Parse.Cloud.define to define as many cloud functions as you want.
+// // For example:
+// Parse.Cloud.define("addTestPlayer", function(request, response) {
+//   response.success(addPlayer('player3', 45));
+// });
+// var totalsLog = {};
 
-Parse.Cloud.define("averageScore", function(request, response) {
-  var query = new Parse.Query("test");
-  // query.equalTo("player", request.params.movie);
+// Parse.Cloud.define("averageScore", function(request, response) {
+//   var query = new Parse.Query("test");
+//   // query.equalTo("player", request.params.movie);
+//   query.find({
+//     success: function(results) {
+//       var sum = 0;
+//       for (var i = 0; i < results.length; i += 1) {
+//         sum += results[i].get("score");
+//       }
+//       totalsLog[0] = sum / results.length;
+//       response.success(totalsLog);
+//     },
+//     error: function() {
+//       response.error("score lookup failed");
+//     }
+//   });
+// });
+
+// Parse.Cloud.define("totalScore", function(request, response) {
+//   var query = new Parse.Query("test");
+//   // query.equalTo("player", request.params.movie);
+//   query.find({
+//     success: function(results) {
+//       var sum = 0;
+//       for (var i = 0; i < results.length; i += 1) {
+//         sum += results[i].get("score");
+//       }
+//       totalsLog[1] = sum;
+//       response.success(totalsLog);
+//     },
+//     error: function() {
+//       response.error("score lookup failed");
+//     }
+//   });
+// });
+
+Parse.Cloud.define("totals", function(request, response) {
+
+var totalCoins;
+var totalMinions;
+
+  var query = new Parse.Query("playerEvent");
+  query.equalTo('eventType', 'levelEnd');
   query.find({
     success: function(results) {
-      var sum = 0;
-      for (var i = 0; i < results.length; i += 1) {
-        sum += results[i].get("score");
-      }
-      response.success(sum / results.length);
+    	console.log(results);
+
+		totalCoins = results.reduce(function(a,b){
+			return a + b.get('level').levelCoins;
+		}, 0);
+
+		totalMinions = results.reduce(function(a,b){
+			return a + b.get('level').levelMinions;
+		}, 0);
+
+      
+      response.success([totalCoins, totalMinions]);
     },
     error: function() {
       response.error("score lookup failed");
     }
-  });
-});
+  }).done();
 
-Parse.Cloud.define("totalScore", function(request, response) {
-  var query = new Parse.Query("test");
-  // query.equalTo("player", request.params.movie);
-  query.find({
-    success: function(results) {
-      var sum = 0;
-      for (var i = 0; i < results.length; i += 1) {
-        sum += results[i].get("score");
-      }
-      response.success(sum);
-    },
-    error: function() {
-      response.error("score lookup failed");
-    }
-  });
+var totalsLog = Parse.Object('TotalStats');
+var resetTotals = new Parse.Query(TotalStats);
+
+resetTotals.get("KEpS6v6cfg", {
+  	success: function(totalsLog) {
+  		console.log('totals log is' + totalsLog);
+		totalsLog.set('Coins', totalCoins );
+		totalsLog.set('Minions', totalMinions);
+		totalsLog.save();  			},
+  	error: function(object, error) {
+  	  // The object was not retrieved successfully.
+  	  // error is a Parse.Error with an error code and description.
+  	}
+	});
 });
