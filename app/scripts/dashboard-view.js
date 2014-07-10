09f8ba2d654fcca09b0732d5ce05e86da40547a2
Joshua Smith <joshuawordsmith@gmail.com>
2014-07-10 19:41:52 -0400

    added some dynamic info to dashboard and group page

--- app/scripts/dashboard-view.js
+++ app/scripts/dashboard-view.js
@@ -3,7 +3,7 @@ var DashboardView = Parse.View.extend ({
 	events: {
 		'click	.create-group-button'		: 'createGroupNav', 
 		'click	.compare-groups-button'		: 'compareGroupsNav', 
-		'click	.group-details-container'	: 'groupView', 
+		'click	.dash-group'				: 'groupNav', 
 	},
 
 	template: _.template($('.dashboard-view').text()),
@@ -12,12 +12,45 @@ var DashboardView = Parse.View.extend ({
 
 	initialize: function () {
 		$('.app-container').append(this.el);
+		this.getGroups();
 		this.render();
 	},
 
 	render: function() {
 		var renderedTemplate = this.template;
 		this.$el.html(renderedTemplate);
+	},
+
+	getGroups: function() {
+		var Groups = Parse.Object.extend("Groups");
+		var query = new Parse.Query(Groups);
+		// checking for group objects made by the current user
+		query.equalTo("orgName", Parse.User.current().attributes.username);
+		query.find({
+			success: function(userGroups) {
+				var renderedTemplate = _.template($('.dashboard-group-view').text())
+				userGroups.forEach(function(userGroup){
+					var GroupTotals = Parse.Object.extend("GroupTotals");
+					var query = new Parse.Query(GroupTotals);
+					// checking for GroupTotals objects for the current users groups
+					query.equalTo("groupID", userGroup.attributes.groupID);
+					query.find({
+						success: function(groupTotal) {
+							$('.dashboard-group-content').append(renderedTemplate(groupTotal[0].attributes));
+
+						},
+						error: function(error) {
+							console.log(error)
+						}
+					})
+				})
+			},
+			error: function(error) {
+				console.log(error)
+			},
+
+		})
+
 	},
 
 	createGroupNav: function() {
@@ -28,8 +61,10 @@ var DashboardView = Parse.View.extend ({
 		router.navigate('/#tournament/dashboard/compare-groups', {trigger: true});		
 	},
 
-	groupView: function() {
-		// router.navigate('/#tournament/group/123', {trigger: true});		
+	groupNav: function(location) {
+		
+		var groupNav = location.currentTarget.children[0].children[0].children[1].innerHTML;
+		router.navigate('/#tournament/group/' + groupNav, {trigger: true});		
 	},
 
 })
